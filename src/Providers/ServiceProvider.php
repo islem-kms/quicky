983ac9310b30a1a28cde663b331a699e25904b32
Islem Khemissi <khemissi.islem@gmail.com>
2019-02-13 15:33:16 +0100

    service provider

---
+++ src/Providers/ServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Laraquick\Providers;
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
